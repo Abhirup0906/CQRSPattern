322a947d00bd7776c92b22df9334aa5c754dea58
Abhirup Sadhukhan <abhirupsadhukhan@gmail.com>
2021-03-29 11:32:19 +0530

    Lazy implementation added.

--- CQRSPattern.Library/DIConfig.cs
+++ CQRSPattern.Library/DIConfig.cs
@@ -16,8 +16,11 @@ namespace CQRSPattern.Library
         public static void Register(IServiceCollection services)
         {
             services.AddScoped<IEmployeeRepository, EmployeeRepository>();
-            services.AddScoped<ICommandHandler<BaseResponseModel, EmployeeModel>, CreateEmployeeHandler>();
-            services.AddScoped<ICommandHandler<IEnumerable<EmployeeModel>, BaseRequestModel>, GetEmployeeHandler>();
+            services.AddScoped<ICommandHandler<BaseResponseModel, EmployeeModel>, CreateEmployeeHandler>()
+                .AddScoped(serviceProvider => new Lazy<ICommandHandler<BaseResponseModel, EmployeeModel>>(() => serviceProvider.GetService<ICommandHandler<BaseResponseModel, EmployeeModel>>()));
+
+            services.AddScoped<ICommandHandler<IEnumerable<EmployeeModel>, BaseRequestModel>, GetEmployeeHandler>()
+                .AddScoped(serviceProvider => new Lazy<ICommandHandler<IEnumerable<EmployeeModel>, BaseRequestModel>>(()=> serviceProvider.GetService<ICommandHandler<IEnumerable<EmployeeModel>, BaseRequestModel>>()));            
 
             services.AddMediatR(typeof(DIConfig).Assembly);
         }
--- CQRSPattern/Controllers/EmployeeController.cs
+++ CQRSPattern/Controllers/EmployeeController.cs
@@ -14,12 +14,17 @@ namespace CQRSPattern.Controllers
     [ApiController]
     public class EmployeeController : ControllerBase
     {
+        /*
+         * Lazy initialization is added. 
+         * For repo level object we can create using Lazy but its not recommendate, instead of this we can use object pooling or singleton etc.
+         * Lazy is good for heavy objects like (Database level operation, file operation or httpClient etc.)
+         */ 
         private readonly IMediator _mediator;
-        private readonly ICommandHandler<BaseResponseModel, EmployeeModel> _createEmployee;
-        private readonly ICommandHandler<IEnumerable<EmployeeModel>, BaseRequestModel> _getEmployee;
+        private readonly Lazy<ICommandHandler<BaseResponseModel, EmployeeModel>> _createEmployee;
+        private readonly Lazy<ICommandHandler<IEnumerable<EmployeeModel>, BaseRequestModel>> _getEmployee;
         public EmployeeController(IMediator mediatr, 
-            ICommandHandler<BaseResponseModel, EmployeeModel> createEmployee,
-            ICommandHandler<IEnumerable<EmployeeModel>, BaseRequestModel> getEmployee)
+            Lazy<ICommandHandler<BaseResponseModel, EmployeeModel>> createEmployee,
+            Lazy<ICommandHandler<IEnumerable<EmployeeModel>, BaseRequestModel>> getEmployee)
         {
             _mediator = mediatr;
             _createEmployee = createEmployee;
@@ -29,7 +34,7 @@ namespace CQRSPattern.Controllers
         [HttpGet("GetEmployee")]
         public IActionResult GetEmployee()
         {
-            return new JsonResult(_getEmployee.Handle(new BaseRequestModel()));
+            return new JsonResult(_getEmployee.Value.Handle(new BaseRequestModel()));
         }
 
         [HttpGet("GetMediatREmployee")]
@@ -41,7 +46,7 @@ namespace CQRSPattern.Controllers
         [HttpPost("CreateEmployee")]
         public IActionResult CreateEmployee([FromBody]EmployeeModel employeeModel)
         {
-            return new JsonResult(_createEmployee.Handle(employeeModel));
+            return new JsonResult(_createEmployee.Value.Handle(employeeModel));
         }
 
         [HttpPost("CreateMediatREmployee")]
